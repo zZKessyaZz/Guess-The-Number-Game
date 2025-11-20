43e1dbea7297c76726ae46882c9ae8ebadd25188
Késsya Cavalcanti <kessyacavalcanti2017@gmail.com>
2025-11-20 12:20:43 -0300

    Janela atual

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import tkinter as tk
 import random
 
+#Janela
 janela = tk.Tk()
 janela.title("Guess The Number Game")
 janela.geometry("500x400")
 janela.configure(bg="gray")
 
+#Label principal
 titulo = tk.Label(janela, text="Adivinhe o Número (1 a 100)", font=("Arial",14,"bold"), bg = "gray")
 titulo.pack(pady = 20)
+
 
 
 
